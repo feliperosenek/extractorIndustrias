70b1feed7af9d57dd0a4e5e33d090c6f6131ffb2
Felipe Rosene Klapsztein <feliperosenek@gmail.com>
2023-03-07 13:20:37 -0300

    Update cnpjApi.js

--- cnpjApi.js
+++ cnpjApi.js
@@ -1,5 +1,5 @@
 const consultarCNPJ = require('consultar-cnpj')
-const token = 'y70p3Lp4SZpAccKaNmFINrBJUWWz5D396OhIsMCm4bMX'
+const token = 'oNGyLzExsCUUh40O8azhX4aHn0iLL6xC6xjVNvgVV33w'
 const puppeteer = require('puppeteer');
 const Sequelize = require('sequelize');
 const {
